c0f8d4c4bfdfc07e1258dca2858f8c5a30b442f5
zhangyong <zhangyong7120180@163.com>
2016-01-07 16:22:48 +0800

    添加 客户端工厂

---
+++ rpc-factory/src/main/java/com/ucar/rpc/factory/RpcClientFactory.java
@@ -0,0 +1,17 @@
+package com.ucar.rpc.factory;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * rpc 客户端工厂
+ * User: zhangyong
+ * Date: 2016/1/7
+ * Time: 16:09
+ * To change this template use File | Settings | File Templates.
+ */
+public class RpcClientFactory {
+
+    private final static Logger logger = LoggerFactory.getLogger(RpcClientFactory.class);
+
+}
