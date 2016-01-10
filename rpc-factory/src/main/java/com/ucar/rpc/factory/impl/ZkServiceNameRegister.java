716c11f93b4f1f57cde909d25f972f69430cb50a
zhangyong <zhangyong7120180@163.com>
2016-01-10 13:23:23 +0800

    先写到这里 ，要去吃饭了。

--- rpc-factory/src/main/java/com/ucar/rpc/factory/impl/ZkServiceNameRegister.java
+++ rpc-factory/src/main/java/com/ucar/rpc/factory/impl/ZkServiceNameRegister.java
@@ -21,11 +21,15 @@ public class ZkServiceNameRegister implements ServiceNameRegister {
     public void registerService() throws Exception {
         logger.info("开始在集群中注册该节点:{} ", zkServiceNameConfig.getClusterNode());
         zkClient = new ZkClient(zkServiceNameConfig.getZkHosts(), zkServiceNameConfig.getZkSessionTimeout(), zkServiceNameConfig.getZkConnectTimeout(), new ZkUtils.StringSerializer());
-        ZkUtils.makeSurePersistentPathExists(zkClient , zkServiceNameConfig.getZkClusterRoot());
+        //确认根目录存在
+        ZkUtils.makeSurePersistentPathExists(zkClient, zkServiceNameConfig.getZkClusterRoot());
+        //确认当前服务的节点
+        ZkUtils.makeSurePersistentPathExists(zkClient, zkServiceNameConfig.getZkClusterRoot() + "/" + zkServiceNameConfig.getClusterNode());
+        //创建临时节点
     }
 
     @Override
-    public void unRegisterService() throws Exception{
+    public void unRegisterService() throws Exception {
 
     }
 
